8a39ada961a2597edf91e1791b3fd5c8b75dbda4
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-07 11:23:14 +0100

    exemples binding

--- src/app/components/first/first.component.ts
+++ src/app/components/first/first.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
-  styleUrls: ['./first.component.css']
+  styleUrls: ['./first.component.css'],
 })
 export class FirstComponent {
+  name = 'aymen';
+  isHidden = false;
+  message = '';
 
+  /**
+   * Elle permet d'afficher ou
+   * de cacher l'élément ciblé
+   * par isHidden
+   */
+  showHide() {
+    this.isHidden = !this.isHidden;
+  }
+
+  changeMessage(newMessage: string) {
+    this.message = newMessage;
+  }
 }
