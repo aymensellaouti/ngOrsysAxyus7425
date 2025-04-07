2b7f6e09eb0cc784dfe0f42ad5cd28f38ebccac3
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-07 09:39:16 +0100

    mise en place de bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Pour appeler ce composant va dans le html du parent
+  // et tappe <app-root/>
   selector: 'app-root',
+  // C'est le bout de code HTML que je gére
   templateUrl: './app.component.html',
+  //  C'est les fichiers css qui mette en page mon fichier html
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
