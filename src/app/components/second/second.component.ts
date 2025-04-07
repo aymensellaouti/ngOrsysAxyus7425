3c6d3304a772d32c8170515be02fb5c325e0b69a
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-07 10:44:11 +0100

    add first et second component

---
+++ src/app/components/second/second.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: `<div class="alert alert-secondary">
+    <p>
+      Cc je suis le second component
+    </p>
+  </div>`,
+  styles: [``]
+})
+export class SecondComponent {
+
+}
