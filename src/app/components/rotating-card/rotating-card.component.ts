44d9fbe3a5cfa11cfbab1fd08ff8e19362365b43
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-07 13:54:51 +0100

    correction rotating card

--- src/app/components/rotating-card/rotating-card.component.ts
+++ src/app/components/rotating-card/rotating-card.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-rotating-card',
   templateUrl: './rotating-card.component.html',
-  styleUrls: ['./rotating-card.component.css']
+  styleUrls: ['./rotating-card.component.css'],
 })
 export class RotatingCardComponent {
-
+  // Le state de notre composant
+  name = 'Sellaouti';
+  firstname = 'Aymen';
+  job = 'teacher';
+  path = 'rotating_card_profile3.png';
 }
